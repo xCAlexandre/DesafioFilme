e5d0696831e97b9de75d2d948dcb20fba1e41b49
João Ricardo Favan <rickfavan@gmail.com>
2021-04-07 23:00:45 -0300

    Semeadura de dados

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,15 +1,22 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Bcc.Models
 {
     public class Movie
     {
         public int Id {get; set;}
+        
         public string Title {get; set;}
+        
+        [Display(Name = "Data de Lançamento")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate {get; set;}
+        
         public string Genre {get; set;}
+        
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price {get; set;}
     }
 }
---
+++ Models/SeedData.cs
@@ -0,0 +1,60 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Bcc.Data;
+using System;
+using System.Linq;
+
+namespace Bcc.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using(var context = new BccContext(
+                serviceProvider.GetRequiredService<
+                DbContextOptions<BccContext>>()))
+            {
+                // Ver se há filmes cadastrados
+                if(context.Movie.Any())
+                {
+                    return; //retorno os filmes cadastrados
+                }
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Interstelar",
+                        ReleaseDate = DateTime.Parse("2014-11-6"),
+                        Genre = "Ficção Científica",
+                        Price = 14.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Como Treinar Seu Dragão 2",
+                        ReleaseDate = DateTime.Parse("2010-3-26"),
+                        Genre = "Animação",
+                        Price = 10M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Luca",
+                        ReleaseDate = DateTime.Parse("2021-6-16"),
+                        Genre = "Animação",
+                        Price = 133M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Bastardos Inglorios",
+                        ReleaseDate = DateTime.Parse("2009-10-9"),
+                        Genre = "Ação",
+                        Price = 30M
+                    }
+
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}

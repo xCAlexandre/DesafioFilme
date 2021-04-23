4c5814bfe8cc9c677ae6a3613169d34df2fd44a5
João Ricardo Favan <rickfavan@gmail.com>
2021-04-22 21:07:25 -0300

    Validacao

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -8,17 +8,27 @@ namespace Bcc.Models
     {
         public int Id {get; set;}
         
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title {get; set;}
         
         [Display(Name = "Data de Lançamento")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate {get; set;}
         
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre {get; set;}
         
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price {get; set;}
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
         public string Rating {get; set;}
     }
 }

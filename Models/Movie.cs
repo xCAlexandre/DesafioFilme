94921ee9d4ad75c3090ca86a624d7c5549d63b96
João Ricardo Favan <rickfavan@gmail.com>
2021-04-28 23:01:58 -0300

    Validacao Finalizada

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -8,27 +8,19 @@ namespace Bcc.Models
     {
         public int Id {get; set;}
         
-        [StringLength(60, MinimumLength = 3)]
-        [Required]
+        [StringLength(60, MinimumLength = 3), Required]
         public string Title {get; set;}
         
-        [Display(Name = "Data de Lançamento")]
-        [DataType(DataType.Date)]
+        [Display(Name = "Data de Lançamento"), DataType(DataType.Date)]
         public DateTime ReleaseDate {get; set;}
         
-        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
-        [Required]
-        [StringLength(30)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$"), Required, StringLength(30)]
         public string Genre {get; set;}
-        
-        [Range(1,100)]
-        [DataType(DataType.Currency)]
-        [Column(TypeName = "decimal(18, 2)")]
+      
+        [Range(1,100), DataType(DataType.Currency), Column(TypeName = "decimal(18, 2)")]
         public decimal Price {get; set;}
 
-        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
-        [StringLength(5)]
-        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5), Required]
         public string Rating {get; set;}
     }
 }
